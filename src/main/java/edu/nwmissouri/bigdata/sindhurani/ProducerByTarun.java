81d6c743a0c219274daa43cec9e7904d8aaffb51
Tarun Sarpanjeri <tarun.sarpanjeri@gmail.com>
2021-03-02 11:51:13 -0600

    Added initial code

---
+++ src/main/java/edu/nwmissouri/bigdata/sindhurani/ProducerByTarun.java
@@ -0,0 +1,45 @@
+package edu.nwmissouri.bigdata.sindhurani;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class ProducerByTarun{
+	 private static Scanner in;
+
+	  public static void main(String[] argv) throws Exception {
+	    if (argv.length != 1) {
+	      System.err.println("Please specify 1 parameter (the name of the topic)");
+	      System.exit(-1);
+	    }
+	    String topicName = argv[0];
+	    in = new Scanner(System.in);
+	    System.out.println("Thank you for providing the topic " + topicName + "\n");
+	    System.out.println("Enter message (type exit to quit).\n");
+
+	   
+	    Properties configProperties = new Properties();
+	    configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+	    configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+	        "org.apache.kafka.common.serialization.ByteArraySerializer");
+	    configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+	        "org.apache.kafka.common.serialization.StringSerializer");
+	    System.out.println("The configuration properties are: " + configProperties.toString());
+	    System.out.println("\nWill use this configuration to create a producer.\n");
+
+	    org.apache.kafka.clients.producer.Producer producer = new KafkaProducer(configProperties);
+
+	   
+   
+	    
+	   
+	    
+	  }
+	  
+	  
+}
